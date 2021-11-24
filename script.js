62e0ab40a6d1c57922de1f46fcc644b4636f0d0e
SergeySR95 <sklyarov-sergey95@mail.ru>
2021-11-25 01:41:22 +0300

    Поле для ввода пароля с переключением видимости пароля

--- script.js
+++ script.js
@@ -18,3 +18,18 @@ post.addEventListener("dblclick", () => {
     heart.classList.remove("animate-like");
   }, 800);
 });
+
+// Поле для ввода пароля с переключением видимости пароля
+
+function togglePassword() {
+  var input = document.getElementById("password");
+  var icon = document.getElementById("icon");
+
+  if (input.type === "password") {
+    input.type = "text";
+    icon.classList.add("selected");
+  } else {
+    input.type = "password";
+    icon.classList.remove("selected");
+  }
+}
